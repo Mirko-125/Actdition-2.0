ebf35b69e02e1a5d1f049ce92d0ad6407e22bed1
Mirko <mirkoat21@gmail.com>
2024-11-25 20:08:58 +0100

    Default margin fixed + navbar is visible

--- actdition-visible/src/components/functionalities/navbar/Navbar.jsx
+++ actdition-visible/src/components/functionalities/navbar/Navbar.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import styles from './Navbar.module.css';
 
 function Navbar() {
-    <>
-        <nav className={styles.navbar}>
-            <ul className={styles.navigationlinks}>
-                <li className={styles.link}><a onClick={() => navigate('/')} className={styles.clickablelink}>How it works</a></li>
-                <li className={styles.link}><a onClick={() => navigate('/')} className={styles.clickablelink}>Sign in</a></li>
-            </ul>
-        </nav>
-    </>
+    
+    return (
+        <>    
+            <nav className={styles.navbar}>
+                <ul className={styles.navigationlinks}>
+                    <li className={styles.link}><a onClick={() => console.log("haha")} className={styles.clickablelink}>How it works</a></li>
+                    <li className={styles.link}><a onClick={() => console.log("haha")} className={styles.clickablelink}>Sign in</a></li>
+                </ul>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar;
