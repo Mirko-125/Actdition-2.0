c8ecb81907af72aab2a367f4502c12d700834a4a
Mirko <mirkoat21@gmail.com>
2024-11-27 22:20:33 +0100

    Better colors

--- actdition-visible/src/components/functionalities/navbar/Navbar.jsx
+++ actdition-visible/src/components/functionalities/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
         <>    
             <nav className={styles.navbar}>
             <li className={styles.link}><a onClick={() => console.log("haha")}><span className={styles.circle} data-color="how-it-works">B</span>How it works</a></li>
-            <li className={styles.link}><a onClick={() => console.log("haha")}><span className={styles.circle} data-color="sign-in">1</span>Sign in</a></li>
+            <li className={styles.link}><a onClick={() => console.log("haha")}><span className={styles.circle} data-color="sign-in">A</span>Sign in</a></li>
             </nav>
         </>
     )
