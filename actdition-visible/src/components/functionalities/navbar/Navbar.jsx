7e5a719d8379c7f4693c3b83d643d7a1eb385977
Mirko <mirkoat21@gmail.com>
2024-11-27 22:11:04 +0100

    Navbar has the New York nomenclature style, and ready to add more functionalities

--- actdition-visible/src/components/functionalities/navbar/Navbar.jsx
+++ actdition-visible/src/components/functionalities/navbar/Navbar.jsx
@@ -6,10 +6,8 @@ function Navbar() {
     return (
         <>    
             <nav className={styles.navbar}>
-                <ul className={styles.navigationlinks}>
-                    <li className={styles.link}><a onClick={() => console.log("haha")} className={styles.clickablelink}>How it works</a></li>
-                    <li className={styles.link}><a onClick={() => console.log("haha")} className={styles.clickablelink}>Sign in</a></li>
-                </ul>
+            <li className={styles.link}><a onClick={() => console.log("haha")}><span className={styles.circle} data-color="how-it-works">B</span>How it works</a></li>
+            <li className={styles.link}><a onClick={() => console.log("haha")}><span className={styles.circle} data-color="sign-in">1</span>Sign in</a></li>
             </nav>
         </>
     )
