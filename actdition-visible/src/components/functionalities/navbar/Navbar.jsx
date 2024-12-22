4d81450e843367a2fbde9a19748d77350ec3a781
Mirko <mirkoat21@gmail.com>
2024-12-22 02:07:39 +0100

    Tiny update

--- actdition-visible/src/components/functionalities/navbar/Navbar.jsx
+++ actdition-visible/src/components/functionalities/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     return (
         <>    
             <nav className={styles.navbar}>
-            <li className={styles.link}><a onClick={() => console.log("haha")}><span className={styles.circle} data-color="how-it-works">B</span>How it works</a></li>
+            <li className={styles.link}><a onClick={() => console.log("haha")}>How it works & About<span className={styles.circle} data-color="how-it-works">B</span><span className={styles.line}></span></a></li>
             <li className={styles.link}><a onClick={() => console.log("haha")}><span className={styles.circle} data-color="sign-in">A</span>Sign in</a></li>
             </nav>
         </>
--- actdition-visible/src/components/looks/showcase/Showcase.jsx
+++ actdition-visible/src/components/looks/showcase/Showcase.jsx
@@ -5,14 +5,19 @@ function Showcase() {
 
   return (
     <>
-        <div className={styles.showcase}>
-            <figure id={styles.showcase}>
-                <section></section>
-                <section></section>  
-                <section></section>  
-                <section></section>  
-            </figure>
-      </div>
+        <div className={styles.group}>
+          <div className={styles.showcase}>
+              <figure id={styles.showcase}>
+                  <section></section>
+                  <section></section>  
+                  <section></section>  
+                  <section></section>  
+              </figure>
+          </div>
+          <div className={styles.info}>
+          For casting directors and producers, Actdition simplifies the talent discovery process. Its intuitive interface allows them to post casting calls, browse candidate profiles, and manage auditions effortlessly. Built-in collaboration features make it seamless to share feedback and make data-driven casting decisions within teams.
+          </div>
+        </div>
     </>
   )
 }
