0732e758b0097e252340f4937f0ead4f51130ead
Mirko <mirkoat21@gmail.com>
2025-03-04 13:41:07 +0100

    Made the home page responsive

--- actdition-visible/src/components/functionalities/navbar/Navbar.jsx
+++ actdition-visible/src/components/functionalities/navbar/Navbar.jsx
@@ -6,8 +6,10 @@ function Navbar() {
     return (
         <>    
             <nav className={styles.navbar}>
-            <li className={styles.link}><a onClick={() => console.log("one day this will be so good")}>How it works & About<span className={styles.circle} data-color="how-it-works">B</span><span className={styles.line}></span></a></li>
-            <li className={styles.link}><a onClick={() => console.log("one day this will be so good")}><span className={styles.circle} data-color="sign-in">A</span>Sign in</a></li>
+                <li className={styles.link}><a onClick={() => console.log("one day this will be so A")}>How it works & About<span className={styles.circle} data-color="how-it-works">B</span><span className={styles.line}></span></a>
+                </li>
+                <li className={styles.link}><a onClick={() => console.log("one day this will be so B")}><span className={styles.circle} data-color="sign-in">A</span>Sign in</a
+                ></li>
             </nav>
         </>
     )
